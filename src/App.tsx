3d6f9bcc3f82faf4c367ffdb8ac3985c29f59975
SISTEMAS-TTA <sistemas@ttarquitectos.mx>
2025-09-10 10:51:37 -0600

    feat: componente login_card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4 py-8">
+      <Card className="w-full max-w-sm sm:max-w-md">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-justify">
+            Login to your account
+          </CardTitle>
+          <CardAction className="text-center">
+            <Button variant="link">Sign Up</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent className="px-6">
+          <form className="space-y-4">
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  className="h-10"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline text-blue-600 dark:text-blue-400"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  className="h-10"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-3 px-6">
+          <Button type="submit" className="w-full h-10 font-medium">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full h-10">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
-export default App
+export default App;
